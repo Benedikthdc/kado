02c49a89165004003a9c504047b2de6888140553
benedikt conze <benedikt@conze.de>
2022-05-31 10:13:05 +0200

    models done

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,4 +3,8 @@ class PagesController < ApplicationController
 
   def home
   end
+
+  def dashboard
+    @projects = current_user.owned_projects + current_user.projects
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :owned_projects, class_name: "Project", foreign_key: :user_id
+  has_many :user_projects
+  has_many :projects, through: :user_projects
 end
