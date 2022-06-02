19e33a0cd14115ab43936f17881d0712ff0a8ab7
benedikt conze <benedikt@conze.de>
2022-06-02 14:17:37 +0200

    votes backend is done

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -1,4 +1,18 @@
 class Idea < ApplicationRecord
   belongs_to :project
   belongs_to :user
+
+  has_many :votes, dependent: :destroy
+
+  def positive_votes
+    votes.where(value: 1).count
+  end
+
+  def negative_votes
+    votes.where(value: -1).count
+  end
+
+  def total_votes
+    positive_votes - negative_votes
+  end
 end
